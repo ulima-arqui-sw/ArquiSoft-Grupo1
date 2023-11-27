0f8c5bddc9d705a60570dfcf3f1b53cc1357839c
DarkShadow76 <alonzotoricaceres@gmail.com>
2023-11-26 23:33:17 -0500

    implementacion de dotenv

--- backend-app/src/db.js
+++ backend-app/src/db.js
@@ -1,14 +1,15 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
 
-const sequelizeOpts = {
-  username: 'postgres',
-  password: 'postgres',
-  database: 'grupo01',
-  dialect: 'postgres',
-  host: "127.0.0.1",
-};
+dotenv.config();
 
-const sequelize = new Sequelize(sequelizeOpts);
-
+const sequelize = new Sequelize({
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
 
 module.exports = sequelize;
