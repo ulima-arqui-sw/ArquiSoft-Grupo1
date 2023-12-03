b88c1b0c8b856143245ef75389b5bfb0b2713d07
DarkShadow76 <alonzotoricaceres@gmail.com>
2023-12-02 21:34:50 -0500

    Model Horario

---
+++ backend-app/src/models/Horario.js
@@ -0,0 +1,33 @@
+import { DataTypes, Sequelize } from "sequelize";
+import { db } from "../db.js"
+
+const horario = db.define('horarios', {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    allowNull: false,
+    defaultValue: Sequelize.UUIDV4
+  },
+  id_mentor: {
+    type: DataTypes.UUID,
+    allowNull: false
+  },
+  dia_semana: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  hora_inicio: {
+    type: DataTypes.TIME,
+    allowNull: false
+  },
+  hora_fin: {
+    type: DataTypes.TIME,
+    allowNull: false
+  },
+}, {
+  tableName: 'Horario',
+  modelName: 'Horario',
+  timestamps: false,
+  freezeTableName: true
+}
+);
